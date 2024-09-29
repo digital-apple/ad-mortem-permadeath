ec7f888c940d401b4225e73e274b77defa8a41d2
digital-apple <115373190+digital-apple@users.noreply.github.com>
2024-09-29 14:08:48 -0300

    Refactor

--- source/Main.cpp
+++ source/Main.cpp
@@ -3,57 +3,57 @@
 
 void InitLogging()
 {
-	auto path = logger::log_directory();
-	if (!path)
-		return;
+    auto path = logger::log_directory();
+    if (!path)
+        return;
 
-	const auto plugin = SKSE::PluginDeclaration::GetSingleton();
-	*path /= std::format("{}.log", plugin->GetName());
+    const auto plugin = SKSE::PluginDeclaration::GetSingleton();
+    *path /= std::format("{}.log", plugin->GetName());
 
-	std::vector<spdlog::sink_ptr> sinks{
-		std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true),
-		std::make_shared<spdlog::sinks::msvc_sink_mt>()
-	};
+    std::vector<spdlog::sink_ptr> sinks{
+        std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true),
+        std::make_shared<spdlog::sinks::msvc_sink_mt>()
+    };
 
-	auto logger = std::make_shared<spdlog::logger>("global", sinks.begin(), sinks.end());
-	logger->set_level(spdlog::level::info);
-	logger->flush_on(spdlog::level::info);
+    auto logger = std::make_shared<spdlog::logger>("global", sinks.begin(), sinks.end());
+    logger->set_level(spdlog::level::info);
+    logger->flush_on(spdlog::level::info);
 
-	spdlog::set_default_logger(std::move(logger));
-	spdlog::set_pattern("[%^%L%$] %v");
+    spdlog::set_default_logger(std::move(logger));
+    spdlog::set_pattern("[%^%L%$] %v");
 }
 
 void InitMessaging()
 {
-	logger::trace("Initializing messaging listener.");
+    logger::trace("Initializing messaging listener.");
 
-	const auto messaging_interface = SKSE::GetMessagingInterface();
+    const auto messaging_interface = SKSE::GetMessagingInterface();
 
-	if (!messaging_interface->RegisterListener([](SKSE::MessagingInterface::Message* a_message) {
-		switch (a_message->type) {
-		case SKSE::MessagingInterface::kInputLoaded:
-			Settings::Load();
-			Addresses::Hook();
-			break;
-		}
+    if (!messaging_interface->RegisterListener([](SKSE::MessagingInterface::Message* a_message) {
+        switch (a_message->type) {
+        case SKSE::MessagingInterface::kInputLoaded:
+            Settings::Load();
+            Addresses::Hook();
+            break;
+        }
 
-		})) {
-		stl::report_and_fail("Failed to initialize message listener.");
-	}
+        })) {
+        stl::report_and_fail("Failed to initialize message listener.");
+    }
 }
 
 SKSEPluginLoad(const SKSE::LoadInterface* a_skse)
 {
-	InitLogging();
+    InitLogging();
 
-	const auto plugin = SKSE::PluginDeclaration::GetSingleton();
+    const auto plugin = SKSE::PluginDeclaration::GetSingleton();
 
-	logger::info("{} v{} is loading...", plugin->GetName(), plugin->GetVersion());
+    logger::info("{} v{} is loading...", plugin->GetName(), plugin->GetVersion());
 
-	SKSE::Init(a_skse);
-	InitMessaging();
+    SKSE::Init(a_skse);
+    InitMessaging();
 
-	logger::info("{} loaded.", plugin->GetName());
+    logger::info("{} loaded.", plugin->GetName());
 
-	return true;
+    return true;
 }
